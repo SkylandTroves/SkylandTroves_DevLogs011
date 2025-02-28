93a7fbee63b33713bb7d5a3ae93ff4725c192390
Diego Murillo <dmurillo@chapman.edu>
2025-02-27 22:23:26 -0800

    wheel speed and orb issues fixed

--- Assets/Scripts/Wheel.cs
+++ Assets/Scripts/Wheel.cs
@@ -24,9 +24,13 @@ public class Wheel : MonoBehaviour
    private void Update()
    {
       StoreDistanceToWheel();
+   }
+
+   private void FixedUpdate()
+   {
       HandleWheelScroll(distancePlayerAndWheel);
    }
-   
+
    private void OnMouseDown()
    {
       Debug.Log("You are now clicking on the wheel ");
@@ -50,7 +54,7 @@ public class Wheel : MonoBehaviour
             {
                // Adjust wheel progress based on scroll input with speed restriction
                float currentProgress = movingPlatform.GetWheelProgress();
-               float maxChange = 0.015f; // Maximum allowed change per frame
+               float maxChange = 0.03f; // Maximum allowed change per frame
                float clampedScrollInput = Mathf.Clamp(scrollInput, -maxChange, maxChange);
                float newProgress = Mathf.Clamp(currentProgress + clampedScrollInput, 0, 1);
 
