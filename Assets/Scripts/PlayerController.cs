853cc7b3b510471980926577a22b9ef8983d7685
Kevin <kevkevin07@gmail.com>
2025-04-09 00:38:16 -0700

    New Sounds

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,6 +13,7 @@ public class PlayerController : MonoBehaviour
 	[SerializeField] private GameObject clickParticle;
 	[SerializeField] private Animator stAnimator;
 	[SerializeField] private AudioClip OnClickSFX;
+
 	private List<PickUpController> pickUps;
 	private NavMeshAgent navAgent;
 	private Action onDestinationReached;
@@ -121,7 +122,8 @@ public class PlayerController : MonoBehaviour
 				// instantiate instance of particle effect
 				Instantiate(clickParticle, hit.point, Quaternion.identity);
 				
-				SoundController.instance.PlaySFX(OnClickSFX, transform, 1f);
+				//SoundController.instance.PlaySFX(OnClickSFX, transform, 1f);
+				SoundController.instance.PlaySFX(SoundController.instance.ClickSFX, transform, 1f);
 			}
 			else
 			{
@@ -213,12 +215,14 @@ public class PlayerController : MonoBehaviour
 		isMoving = navAgent.velocity.sqrMagnitude > 0.001f && navAgent.remainingDistance > navAgent.stoppingDistance;
 		//stAnimator.SetBool(isWalking, isMoving);
 	}
-	private void HandlePickUpOrbStart()
+	public void HandlePickUpOrbStart()
 	{
+		SoundController.instance.PlaySFX(SoundController.instance.PickUpBallSFX, transform, 1f);
 		//stAnimator.SetTrigger(pickedUpOrb);
 	}
-	private void HandlePickUpOrbEnd()
+	public void HandlePickUpOrbEnd()
 	{
+		SoundController.instance.PlaySFX(SoundController.instance.DropBallSFX, transform, 1f);
 		//stAnimator.SetTrigger(droppedOrb);
 	}
 	private void UpdateMovementState()
--- Assets/Scripts/SoundController.cs
+++ Assets/Scripts/SoundController.cs
@@ -1,20 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 public class SoundController : MonoBehaviour
 {
     public static SoundController instance;
     [SerializeField] private AudioSource soundFXObject;
     [SerializeField] private AudioSource loopedSFXObject;
+    [SerializeField] private AudioClip pickUpBallSFX;
+    [SerializeField] private AudioClip dropBallSFX;
+    [SerializeField] private AudioClip clickSFX;
+    
+    public AudioClip PickUpBallSFX => pickUpBallSFX;
+    public AudioClip DropBallSFX => dropBallSFX;
+    public AudioClip ClickSFX => clickSFX;
+    
     private void Awake()
     {
         if (instance == null)
         {
             instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
         }
     }
+
+    
     public void PlaySFX(AudioClip audioClip, Transform spawnTransform, float volume = 1f)
     {
+  
         // spawn in the gameObject
         AudioSource audioSource = Instantiate(soundFXObject, spawnTransform.position, Quaternion.identity);
         // assign the clip
@@ -28,15 +45,19 @@ public class SoundController : MonoBehaviour
         // destroy the clip after it is done playing
         Destroy(audioSource.gameObject, clipLength);
     }
+    
     public AudioSource PlayLoopingSFXInstance(AudioClip clip, Transform spawnTransform, float volume = 1f)
-    {
+    {    
         AudioSource audioSource = Instantiate(loopedSFXObject, spawnTransform.position, Quaternion.identity);
         audioSource.clip = clip;
         audioSource.volume = volume;
         audioSource.loop = true;
         audioSource.Play();
+        DontDestroyOnLoad(audioSource.gameObject);
+        
         return audioSource;
     }
+    
     public void StopLoopingSFX()
     {
         if (loopedSFXObject != null)
