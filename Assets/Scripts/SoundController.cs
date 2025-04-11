4d34e7336bb3396da836a44070d6be78de0a08cc
lsunn <laurenhsun@gmail.com>
2025-04-11 10:56:27 -0700

    merge conflict

--- Assets/Scripts/SoundController.cs
+++ Assets/Scripts/SoundController.cs
@@ -1,19 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class SoundController : MonoBehaviour
 {
     public static SoundController instance;
-    [SerializeField] private AudioSource soundFXObject;
-    [SerializeField] private AudioSource loopedSFXObject;
+    [SerializeField] private AudioSource ClickSFXObject;
+    [SerializeField] private AudioSource chargeAreaSFXObject;
     [SerializeField] private AudioClip pickUpBallSFX;
     [SerializeField] private AudioClip dropBallSFX;
-    [SerializeField] private AudioClip clickSFX;
     
     public AudioClip PickUpBallSFX => pickUpBallSFX;
     public AudioClip DropBallSFX => dropBallSFX;
-    public AudioClip ClickSFX => clickSFX;
     
     private void Awake()
     {
@@ -33,7 +32,7 @@ public class SoundController : MonoBehaviour
     {
   
         // spawn in the gameObject
-        AudioSource audioSource = Instantiate(soundFXObject, spawnTransform.position, Quaternion.identity);
+        AudioSource audioSource = Instantiate(ClickSFXObject, spawnTransform.position, Quaternion.identity);
         // assign the clip
         audioSource.clip = audioClip;
         // assign volume
@@ -48,7 +47,7 @@ public class SoundController : MonoBehaviour
     
     public AudioSource PlayLoopingSFXInstance(AudioClip clip, Transform spawnTransform, float volume = 1f)
     {    
-        AudioSource audioSource = Instantiate(loopedSFXObject, spawnTransform.position, Quaternion.identity);
+        AudioSource audioSource = Instantiate(chargeAreaSFXObject, spawnTransform.position, Quaternion.identity);
         audioSource.clip = clip;
         audioSource.volume = volume;
         audioSource.loop = true;
@@ -60,9 +59,9 @@ public class SoundController : MonoBehaviour
     
     public void StopLoopingSFX()
     {
-        if (loopedSFXObject != null)
+        if (chargeAreaSFXObject != null)
         {
-            loopedSFXObject.Stop();
+            chargeAreaSFXObject.Stop();
         }
         else
         {
